79fb09cc01a48714a78557f5ee16e5f9b49d9fd6
ChenShuang <chenshuang.com>
2022-06-21 20:29:17 +0800

    first commit

---
+++ src/main/java/com/chenshuang/GitIdeaApplication.java
@@ -0,0 +1,14 @@
+package com.chenshuang;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class GitIdeaApplication {
+
+    public static void main(String[] args) {
+        //SpringApplication.run(GitIdeaApplication.class, args);
+        System.out.println("hello git");
+    }
+
+}
